823084a811281f73b5d57ab8a5b0f52f6f9147a0
masonxmh <55073376+masonxmh@users.noreply.github.com>
2019-12-08 21:29:08 -0800

    upload

---
+++ ETL.sql
@@ -0,0 +1,23 @@
+-- Create Beer ID Table--------------------------
+DROP TABLE IF EXISTS beer_id_table;
+CREATE TABLE beer_id_table (
+	id INT Not Null Primary Key,
+	name VARCHAR);
+SELECT * FROM beer_id_table;
+
+-- Create Brewery ID Table--------------------------
+DROP TABLE IF EXISTS brewery_id_table;
+CREATE TABLE brewery_id_table (
+	brewery_id INT Not Null Primary Key,
+	name VARCHAR);
+SELECT * FROM brewery_id_table;
+
+
+DROP TABLE IF EXISTS beer_profile_table;
+CREATE TABLE beer_profile_table (
+	id INT Not Null,
+	name VARCHAR ,
+	style VARCHAR,
+	abv float,
+	availability VARCHAR);
+SELECT * FROM beer_profile_table;
