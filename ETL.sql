9c126ea5f4ed59e03178340b6e7c32e7c320d007
masonxmh <55073376+masonxmh@users.noreply.github.com>
2019-12-09 19:06:03 -0800

    finish clean table

--- ETL.sql
+++ ETL.sql
@@ -1,24 +1,52 @@
 -- Create Beer ID Table--------------------------
 DROP TABLE IF EXISTS beer_id_table;
 CREATE TABLE beer_id_table (
-	id INT Not Null Primary Key,
-	name VARCHAR);
+	id INT NOT NULL PRIMARY Key,
+	name VARCHAR NOT NULL);
 SELECT * FROM beer_id_table;
 
 -- Create Brewery ID Table--------------------------
 DROP TABLE IF EXISTS brewery_id_table;
 CREATE TABLE brewery_id_table (
-	brewery_id INT Not Null Primary Key,
-	name VARCHAR);
+	brewery_id INT NOT NULL PRIMARY Key,
+	brewery_name VARCHAR NOT NULL);
 SELECT * FROM brewery_id_table;
 
-
+-- Create Profile Table--------------------------
 DROP TABLE IF EXISTS beer_profile_table;
 CREATE TABLE beer_profile_table (
-	id INT Not Null,
-	name VARCHAR ,
-	style VARCHAR,
-	abv float,
+	id INT NOT NULL,
+	name VARCHAR NOT NULL,
+	beer_style VARCHAR,
+	alchol_by_volume FLOAT,
 	availability VARCHAR,
-	FOREIGN KEY (id) REFERENCES beer_id_table(id),);
+	state VARCHAR,
+	brewery_name VARCHAR,
+	FOREIGN KEY (id) REFERENCES beer_id_table(id));
 SELECT * FROM beer_profile_table;
+
+-- Create Brewery Location Info Table----------------------
+DROP TABLE IF EXISTS brewery_location_table;
+CREATE TABLE brewery_location_table (
+	brewery_id INT NOT NULL,
+	brewery_name VARCHAR NOT NULL,
+	city VARCHAR,
+	state VARCHAR,
+	country VARCHAR,
+	FOREIGN KEY (brewery_id) REFERENCES brewery_id_table(brewery_id));
+SELECT * FROM brewery_location_table;
+
+--Create Beer Reviews Table----------------------------------
+DROP TABLE IF EXISTS review_table;
+CREATE TABLE review_table (
+	id INT NOT NULL,
+	name VARCHAR NOT NULL,
+	beer_style VARCHAR,
+	alchol_by_volume FLOAT,
+	review_overall FLOAT,
+	review_aroma FLOAT,
+	review_appearance FLOAT,
+	review_taste FLOAT,
+	review_time DATE,
+	FOREIGN KEY (id) REFERENCES beer_id_table(id));
+	SELECT * FROM review_table;
